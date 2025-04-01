e04e4ec1859f46e8f377dd8e7f9b3a47a5651264
SeiferWolfDragon <diego.vazquez.1994@hotmail.com>
2025-04-01 00:36:37 +0000

    1.2.5

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #cambio  tag v1.2.3
 #cambio tag v1.2.4
+#cambio tag v1.2.5
 import os
 from flask import Flask, request, jsonify, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
